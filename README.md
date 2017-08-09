a826a35cd7ff70ac86cd767f90eb7d6c3d10ec50
Thomas Bigger <thomasbigger584@gmail.com>
2017-08-09 23:39:19 +0100

    updated gitignore

--- README.md
+++
@@ -1,2 +0,0 @@
-# SpringGenerator
-Generates entities and relationships for a SpringBoot, JPA, MYSQL backend
--- src/main/java/SpringClassGenerator.java
+++
@@ -1,8 +0,0 @@
-public class SpringClassGenerator {
-
-    public static void main(String... args) {
-
-        PropertiesLoader propertiesLoader = new PropertiesLoader();
-        propertiesLoader.loadUserConfig();
-    }
-}
