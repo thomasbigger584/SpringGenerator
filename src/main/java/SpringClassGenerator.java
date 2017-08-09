b34b5a02d53729c72c5f74c5afddba17b9bb5250
Thomas Bigger <thomasbigger584@gmail.com>
2017-08-09 23:30:15 +0100

    initial commit

---
+++ src/main/java/SpringClassGenerator.java
@@ -0,0 +1,8 @@
+public class SpringClassGenerator {
+
+    public static void main(String... args) {
+
+        PropertiesLoader propertiesLoader = new PropertiesLoader();
+        propertiesLoader.loadUserConfig();
+    }
+}
